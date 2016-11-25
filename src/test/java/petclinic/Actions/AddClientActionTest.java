267801224027d6ebd2ad2cd74a7c32e556f94a2e
k0r0tk0ff <korotkov34reg@gmail.com>
2016-11-25 11:22:43 +0300

    end two test

--- src/test/java/petclinic/Actions/AddClientActionTest.java
+++ src/test/java/petclinic/Actions/AddClientActionTest.java
@@ -1,7 +1,14 @@
 package petclinic.Actions;
 
 import org.junit.Test;
+import petclinic.Client;
+import petclinic.Data.Data;
+import petclinic.IO.ConsoleIO;
 
+import java.util.Scanner;
+import java.util.concurrent.CopyOnWriteArrayList;
+
+import static org.hamcrest.core.IsEqual.equalTo;
 import static org.junit.Assert.*;
 
 /**
@@ -11,16 +18,25 @@ public class AddClientActionTest {
     @Test
     public void exe() throws Exception {
 
+        Data data = new Data();
+        Client client = new Client(
+                0, "name", new CopyOnWriteArrayList<>());
+        data.clients.add(client);
+
+        assertTrue(data.clients.contains(client));
+
     }
 
     @Test
     public void intro() throws Exception {
-
+        String intro = "Add a new client";
+        assertThat("Add a new client", equalTo(intro));
     }
 
     @Test
     public void key() throws Exception {
-
+        int key = 1;
+        assertEquals(1, key);
     }
 
 }
---
+++ src/test/java/petclinic/Actions/AddPetActionTest.java
@@ -0,0 +1,26 @@
+package petclinic.Actions;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * Created by root on 11/25/16.
+ */
+public class AddPetActionTest {
+    @Test
+    public void exe() throws Exception {
+
+    }
+
+    @Test
+    public void intro() throws Exception {
+
+    }
+
+    @Test
+    public void key() throws Exception {
+
+    }
+
+}
